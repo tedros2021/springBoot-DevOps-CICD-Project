c4a85389123b2b7c7d0e67c2ca6ec0dc5069c8c8
Tedros2019 <tedrosrghebru@gmail.com>
2025-02-01 14:47:51 -0600

    This is the first commit.

---
+++ src/main/java/com/ted/SpringBootDevOpsCicdProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ted;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDevOpsCicdProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDevOpsCicdProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ted/SpringBootDevOpsCicdProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ted;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDevOpsCicdProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
